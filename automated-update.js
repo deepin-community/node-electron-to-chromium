bd81ace64f90910f03f3e099b88a47342a762574
Tianyu Chen <sweetyfish@deepin.org>
2023-10-18 14:30:32 +0800

    feat: update node-electron-to-chromium to 1.4.552

--- automated-update.js
+++ automated-update.js
@@ -9,7 +9,7 @@ if (!which('npm')) {
   exit(1);
 }
 
-exec('npm update electron-releases && npm run build && npm test', {silent:true}, function(code, stdout, stderr) {
+exec('npm run build && npm test', {silent:true}, function(code, stdout, stderr) {
   if(code === 1) {
 
     exec('echo "' + stdout + stderr + '"| aha | mail -a "Content-type: text/html" -s "[e2c] Automated update failed" root');
@@ -25,7 +25,7 @@ exec('npm update electron-releases && npm run build && npm test', {silent:true},
     exec('git status --porcelain', {silent:true}, function(code, stdout, stderr) {
       const indexHasUpdated = !!stdout.length;
       if(indexHasUpdated) {
-        exec('git add versions.js full-versions.js chromium-versions.js full-chromium-versions.js', {silent:true});
+        exec('git add versions.js full-versions.js chromium-versions.js full-chromium-versions.js versions.json full-versions.json chromium-versions.json full-chromium-versions.json', {silent:true});
         exec('git commit -m "generate new version"', {silent:true});
         exec('npm version patch', {silent:true});
         exec('git push origin master', {silent:true});
